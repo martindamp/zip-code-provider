31e09320f0394023c0c829cdfe47ce10f8217a20
martindamp <martindamp@gmail.com>
2023-10-31 18:40:40 +0100

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,34 @@
+radio.setTransmitPower(7)
+radio.setGroup(2)
+basic.forever(function () {
+    radio.sendString("RZXPSNWZXPSN")
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(5000)
+})
